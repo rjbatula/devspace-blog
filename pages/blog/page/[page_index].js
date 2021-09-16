75cd45eda46f7b5cc2b9d5775c089be0050b3e18
Ralph Joshua <rjbatula@yahoo.com>
2021-09-16 10:21:34 +0800

    Generate Static Paths for Pagination

---
+++ pages/blog/page/[page_index].js
@@ -0,0 +1,71 @@
+import Layout from '../../../components/Layout'
+import Link from 'next/link'
+import fs from 'fs'
+import path from 'path'
+import matter from 'gray-matter'
+import Post from '../../../components/Post'
+import { sortByDate } from '../../../utils'
+import { POST_PER_PAGE } from '../../../config'
+
+export default function BlogPage({ posts }) {
+	return (
+		<Layout>
+			<h1 className='text-5xl border-b-4 p-5 font-bold'>Blog</h1>
+
+			<div className='grid md:grid-cols-2 lg:grid-cols-3 gap-5'>
+				{posts.map((post, index) => (
+					<Post key={index} post={post} />
+				))}
+			</div>
+		</Layout>
+	)
+}
+
+export async function getStaticPaths() {
+	const files = fs.readdirSync(path.join('posts'))
+
+	const numPages = Math.ceil(files.length / POST_PER_PAGE)
+
+	let paths = []
+
+	for (let i = 1; i <= numPages; i++) {
+		paths.push({
+			params: {
+				page_index: i.toString(),
+			},
+		})
+	}
+
+	return {
+		paths,
+		fallback: false,
+	}
+}
+
+export async function getStaticProps() {
+	const files = fs.readdirSync(path.join('posts'))
+
+	// Create a slug and front matter data
+	const posts = files.map((filename) => {
+		//const slug = filename.split('.')[0]
+		const slug = filename.replace('.md', '')
+
+		const markdownWithMeta = fs.readFileSync(
+			path.join('posts', filename),
+			'utf-8'
+		)
+
+		const { data: frontmatter } = matter(markdownWithMeta)
+
+		return {
+			slug,
+			frontmatter,
+		}
+	})
+
+	return {
+		props: {
+			posts: posts.sort(sortByDate),
+		},
+	}
+}
